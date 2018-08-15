9b211347938ec41d766efbec2d02006f29e4e78b
Noisk8 <juansk8jaramillo@gmail.com>
2018-08-14 20:42:11 -0500

    Update foxdot.sh

--- Debian-Ubuntu/foxdot.sh
+++ Debian-Ubuntu/foxdot.sh
@@ -12,7 +12,9 @@ sudo apt-get install git
 
 sudo apt-get install python-tk
 
-echo Instalando pip
+sudo apt-get install python-setuptools
+
+sudo easy_install pip
 
 sudo apt-get install python-pip
 
@@ -27,72 +29,6 @@ git clone https://github.com/Qirky/FoxDot.git
 cd FoxDot
 
 sudo python setup.py install
-
-echo Instalando Supercollider [NECESARIO PARA FOXDOT]
-
-sudo apt-get install libsndfile1-dev 
-sudo apt install libasound2-dev 
-sudo apt install libavahi-client-dev 
-sudo apt install libicu-dev 
-sudo apt install libreadline6-dev
-sudo apt install libfftw3-dev libxt-dev 
-sudo apt install libudev-dev 
-sudo apt install libcwiid-dev 
-sudo apt install g++
-sudo apt install cmake 
-sudo apt install qt5-default
-sudo apt install qt5-qmake
-sudo apt install qttools5-dev
-sudo apt install qttools5-dev-tools
-sudo apt install qtdeclarative5-dev 
-sudo apt install libqt5webkit5-dev 
-sudo apt install qtpositioning5-dev
-sudo apt install libqt5sensors5-dev l
-sudo apt install ibqt5opengl5-dev 
-sudo apt install jackd
-sudo apt install libjack0 
-sudo apt install build-essential 
-sudo apt install libqt4-dev 
-sudo apt install libqtwebkit-dev 
-sudo apt install libjack-dev 
-sudo apt install libsndfile1-dev 
-sudo apt install libasound2-dev 
-sudo apt install libavahi-client-dev 
-sudo apt install libicu-dev 
-sudo apt install libreadline6-dev
-sudo apt install libfftw3-dev 
-sudo apt install libxt-dev 
-sudo apt install libcwiid-dev 
-sudo apt install pkg-config 
-sudo apt install subversion 
-sudo apt install libudev-dev
-sudo apt install libsndfile-dev
-sudo apt install libx11-dev
-sudo apt install libxt-dev
-
-
-
-wget https://github.com/supercollider/supercollider/releases/download/Version-3.8.0/SuperCollider-3.8.0-Source-linux.tar.bz2
-
-tar -xvf SuperCollider-3.8.0-Source-linux.tar.bz2
-
-cd SuperCollider-Source/
-
-mkdir build
-
-cd build
-
-cmake -DSC_ED=OFF -DSC_EL=OFF -DSC_WII=OFF -DSC_VIM=OFF -DSC_IDE=ON ..
-
-make
-
-sudo make install
-
-sudo ldconfig
-
-echo  Instalando sc3-plugins
-
-sudo apt-get install sc3-plugins
 
 echo instalando qjackctl
 
