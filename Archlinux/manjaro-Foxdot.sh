0ccde017f9125ad84bc13aebcbd50d68f3de34ca
Noisk8 <juansk8jaramillo@gmail.com>
2022-10-23 19:16:29 -0500

    Update manjaro-Foxdot.sh

--- Archlinux/manjaro-Foxdot.sh
+++ Archlinux/manjaro-Foxdot.sh
@@ -2,15 +2,15 @@
 
 echo Script para instalar FoxDot en Manjaro y ArchLinux 
 
-pacman -S python-pip	 
+sudo pacman -S python-pip	 
 
 echo ACTUALIZANDO  pip
 
 sudo pip install --upgrade pip
 
-echo ELIGE LA OPCION 1 2 3 Y PRESIONA ENTER
-echo ELIGE LA OPCION 1 2 3 Y PRESIONA ENTER
-echo ELIGE LA OPCION 1 2 3 Y PRESIONA ENTER
+echo ELIGE LA OPCION 1  Y PRESIONA ENTER
+echo ELIGE LA OPCION 1  Y PRESIONA ENTER
+echo ELIGE LA OPCION 1  Y PRESIONA ENTER
 
 yaourt TKinter
 
@@ -21,9 +21,9 @@ sudo pip install FoxDot
 
 echo DESCARGANDO SUPERCOLLIDER
 
-echo ELIGE LA OPCION 1 Y PRESIONA ENTER
-echo ELIGE LA OPCION 1 Y PRESIONA ENTER
-echo ELIGE LA OPCION 1 Y PRESIONA ENTER
+echo ELIGE LA OPCION 2 Y PRESIONA ENTER
+echo ELIGE LA OPCION 2 Y PRESIONA ENTER
+echo ELIGE LA OPCION 2 Y PRESIONA ENTER
 
 yaourt supercollider
 
