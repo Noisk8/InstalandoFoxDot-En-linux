fc7add92d2b70d69b7463c548dcc592c8fbee308
Noisk8 <juansk8jaramillo@gmail.com>
2023-01-03 21:28:40 -0500

    Update arch-Foxdot.sh

--- Archlinux/arch-Foxdot.sh
+++ Archlinux/arch-Foxdot.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+
 echo Script para instalar FoxDot en ArchLinux 
+
+
+sudo pacman -S --needed base-devel git wget yajl
+
+cd /tmp
+
+git clone https://aur.archlinux.org/package-query.git
+
+cd package-query/
+
+makepkg -si && cd /tmp/
+
+git clone https://aur.archlinux.org/yaourt.git
+
+cd yaourt/
+
+makepkg -si
 
 echo INSTALANDO QJACKCTL 
 
@@ -10,17 +28,13 @@ sudo pacman -S python-pip
 
 echo ACTUALIZANDO  pip
 
-sudo pip install --upgrade pip
+pip install --upgrade pip
 
-echo ELIGE LA OPCION 1  Y PRESIONA ENTER
-echo ELIGE LA OPCION 1  Y PRESIONA ENTER
-echo ELIGE LA OPCION 1  Y PRESIONA ENTER
-
-yaourt TKinter
+sudo pacman -S tk
 
 echo INSTALANDO FOXDOT
 
-sudo pip install FoxDot
+pip install FoxDot
 
 
 echo DESCARGANDO SUPERCOLLIDER
