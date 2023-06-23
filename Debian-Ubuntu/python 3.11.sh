1994d39c8623d5e3d3d6a9bbd7f0180bbbc12b94
Noisk8 <juansk8jaramillo@gmail.com>
2023-06-23 02:54:23 -0500

    python 3.11

---
+++ Debian-Ubuntu/python 3.11.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo Bienvenidos al script para instalar Foxdot en Linux
+
+echo Instalando dependencias
+
+echo Instalando pyhton3-tk y git
+
+sudo apt update
+
+sudo apt install git
+
+sudo apt install python3
+
+sudo apt install python3-tk
+
+sudo apt install python3-setuptools
+
+sudo easy_install pip3
+
+sudo apt-get install python3-pip
+
+
+sudo pip3 install -U setuptools
+
+sudo pip3 install -U wheel
+
+
+git clone https://github.com/TheNuSan/FoxDot.git
+
+cd FoxDot
+
+python setup.py install
+
+
+
+#SuperCollider
+echo Instalando Supercollider NECESARIO PARA FOXDOT
+
+#DependenciasSuperCollider
+echo Instalando dependencias supercollider
+
+sudo apt install supercollider
+
+echo Agregando usuario al grupo audio
+
+sudo adduser $USER audio
+
+
+echo instalando qjackctl
+
+sudo apt-get install qjackctl
+
+
+echo sigue los pasos en → → → → http://foxdot.org/installation/
