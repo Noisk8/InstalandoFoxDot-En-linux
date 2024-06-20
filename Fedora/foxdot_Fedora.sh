f3c2088d793bb7be92afe8a913cf3f67331c2692
noisk8 <juansk8jaramillo@gmail.com>
2024-06-20 13:18:56 -0500

    Foxdot on fedora

---
+++ Fedora/foxdot_Fedora.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash/
+
+echo "Inatalando Python pip tkinter setuptools"
+sudo dnf install python3 -y
+sudo dnf install python3-pip -y
+sudo dnf install python3-tkinter -y
+sudo dnf install python3-setuptools -y
+
+echo "Descagarndo Foxdot"
+git clone https://github.com/TheNuSan/FoxDot.git
+cd FoxDot
+
+echo "configurando FoxDot"
+sudo python setup.py install
+
+echo "Instalando SuperCollider"
+sudo dnf install supercollider -y
+
+echo "Abre superCollider y ejecuta Quarks.install("https://github.com/Qirky/FoxDotQuark.git") para instalar los samples
+
+echo "Reinicia el lenguaje del servidor de superCollider y ejecuta FoxDot.start"
+
+echo "entra ala carpeta FoxDot"
+
+echo "ejecuta python3 -m FoxDot "
